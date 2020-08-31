f499d90b8a4fe75122b3cedb70cdd87029b5ec10
danis <danis@LAPTOP-KAGOBQ8C>
2020-08-31 23:51:55 +0530

    First Spring Data Rest Project

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (1, 'Danish', 'Java');
+insert into alien values (2, 'Manish', 'Android');
+insert into alien values (3, 'Tanish', 'Python');
+insert into alien values (4, 'Ravish', 'C#');
+insert into alien values (5, 'Anish', 'Java');
